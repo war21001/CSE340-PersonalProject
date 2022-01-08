dd55a030a6f8fd05b175e84eb8d87c853d2bd020
war21001 <83742731+war21001@users.noreply.github.com>
2022-01-07 21:18:18 -0700

    week 2

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-//begins server and connects the modules/files
+const express = require('express');
+const bodyParser = require('body-parser');
+const path=require('path');
 
-const http = require('http');
+const app = express();
 
-const routes = require('./routes');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-const server = http.createServer(routes);
+app.use(bodyParser.urlencoded({extended: false}));
 
-server.listen(3000);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) =>{
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+
+app.listen(3000);
