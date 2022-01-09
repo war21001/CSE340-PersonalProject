ebe4e12faf033549691ce796208909279469b329
war21001 <83742731+war21001@users.noreply.github.com>
2022-01-08 20:44:02 -0700

    lesson 2

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,26 @@
 const path=require('path');
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
-router.use('/add-product', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+const products = [];
+
+router.get('/add-product', (req, res, next) => {
+    res.render('add-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        formsCSS: true,
+        productCSS: true,
+        activeAddProduct: true
+    })
 });
 
-router.post('/product', (req, res, next) => {
-    console.log(req.body);
+router.post('/add-product', (req, res, next) => {
+    products.push({title: req.body.title});
     res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,20 @@ const path=require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    const products = adminData.products;
+    res.render('shop', {
+        prods: products, 
+        pageTitle: 'Shop', 
+        path: '/', 
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+    });
 });
 
 module.exports = router;
