8b7b34ae0e8453a4a562d5fbe7e85f72c80e3afe
war21001 <83742731+war21001@users.noreply.github.com>
2022-01-11 22:17:16 -0700

    lesson3
    mvc organization

--- routes/admin.js
+++ routes/admin.js
@@ -1,26 +1,18 @@
 const path=require('path');
 const express = require('express');
 
-const rootDir = require('../util/path');
+// const rootDir = require('../util/path');
+
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-const products = [];
+router.get('/add-product', adminController.getAddProduct);
+router.get('/products', adminController.getProducts);
 
-router.get('/add-product', (req, res, next) => {
-    res.render('add-product', {
-        pageTitle: 'Add Product',
-        path: '/admin/add-product',
-        formsCSS: true,
-        productCSS: true,
-        activeAddProduct: true
-    })
-});
+router.post('/add-product', adminController.postAddProduct);
 
-router.post('/add-product', (req, res, next) => {
-    products.push({title: req.body.title});
-    res.redirect('/');
-});
-
-exports.routes = router;
-exports.products = products;
+router.get('/edit-product/:productId', adminController.getEditProduct);
+router.post('/edit-product', adminController.postEditProduct);
+router.post('/delete-product', adminController.postDeleteProduct);
+module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -2,20 +2,18 @@ const path=require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+
+const shopController = require('../controllers/shop');
+
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    const products = adminData.products;
-    res.render('shop', {
-        prods: products, 
-        pageTitle: 'Shop', 
-        path: '/', 
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
-    });
-});
+router.get('/', shopController.getIndex);
+router.get('/products', shopController.getProducts);
+router.get('/products/:productId', shopController.getProductDetails);
+router.get('/cart', shopController.getCart);
+router.post('/cart', shopController.postCart);
+router.get('/orders', shopController.getOrders);
+router.get('/checkout', shopController.getCheckout);
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
 module.exports = router;
